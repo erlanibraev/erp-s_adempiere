94699662ff98250e3db95ecd6ab9323e7695bf97
a.nurpiisov <a.nurpiisov@localhost>
2012-04-13 18:30:28 +0600

    fix condition with employee, vendor, customer

--- src/org/compiere/model/CallOutBPartner.java
+++ src/org/compiere/model/CallOutBPartner.java
@@ -11,20 +11,39 @@ import org.compiere.util.AdempiereSystemError;
 import org.compiere.util.Env; 
 
 public class CallOutBPartner extends CalloutEngine{
-	public String isEmployee(Properties ctx, int windowNo, GridTab mTab, GridField mField, 
+	public String isWho(Properties ctx, int windowNo, GridTab mTab, GridField mField, 
 			 Object value) throws AdempiereSystemError { 
-
-		Boolean isEmployee = (Boolean) mTab.getValue("IsEmployee");
-		if(isEmployee.equals(true)){
-			int result = JOptionPane.showConfirmDialog(null, "При отметке Бизнес-партнера как служащего, " +
-					"то буду сняты отметки 'Поставщик' и 'Покупатель', поставить отметку?", 
-					"Предупреждение", JOptionPane.YES_NO_OPTION);
-			if(result == JOptionPane.YES_OPTION){
-				  mTab.setValue("IsCustomer", false);
-				  mTab.setValue("IsVendor", false);
-				  mTab.setValue("IsEmployee", true);
-			} else mTab.setValue("IsEmployee", false);	  
-		}
+		
+		String who = mField.getColumnName();
+		boolean value2  = (Boolean) mField.getValue();
+		if(who.equals("IsEmployee")){
+			if(value2) 
+				mTab.setValue("IsEmployee", true);
+			else
+				mTab.setValue("IsEmployee", false);
+			
+			mTab.setValue("IsCustomer", false);
+			mTab.setValue("IsVendor", false);
+			  
+		} else if(who.equals("IsCustomer")){
+			if(value2) 
+				mTab.setValue("IsCustomer", true);
+			else
+				mTab.setValue("IsCustomer", false);
+			
+		  mTab.setValue("IsVendor", false);
+		  mTab.setValue("IsEmployee", false);
+		  
+		} else if(who.equals("IsVendor")){
+			if(value2)
+				mTab.setValue("IsVendor", true);
+			else
+				mTab.setValue("IsVendor", false);
+			
+		  mTab.setValue("IsVendor", true);
+		  mTab.setValue("IsEmployee", false);			
+		} else;
+		
 		return null;
 	}
 }
